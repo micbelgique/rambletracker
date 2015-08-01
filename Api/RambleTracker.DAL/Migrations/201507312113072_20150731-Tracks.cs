6cfdc3cd62446bc85302b1655a1c3e1024a56ae7
Louis Stuyck <stuyck.l@gmail.com>
2015-08-01 09:53:58 +0200

    Dal Modifications

---
+++ Api/RambleTracker.DAL/Migrations/201507312113072_20150731-Tracks.cs
@@ -0,0 +1,16 @@
+namespace RambleTracker.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _20150731Tracks : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
